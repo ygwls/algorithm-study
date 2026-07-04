fd778f9e2dbcb8dfaccecc5e2c85f9e8b472b44f
윤효진 <hyojinee08@naver.com>
2026-07-04 23:37:19 +0900

    [level 1] Title: 과일 장수, Time: 64.58 ms, Memory: 23.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,12 @@
+def solution(k, m, score):
+    answer = 0
+    
+    score.sort(reverse=True)
+    
+    for i in range(0, len(score), m):
+        if i + m > len(score):
+            break
+            
+        answer += score[i + m - 1] * m
+        
+    return answer
