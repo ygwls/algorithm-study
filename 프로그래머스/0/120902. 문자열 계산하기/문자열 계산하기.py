1d3bae6da7b224e6eb6ece4d9403202e67df4573
윤효진 <hyojinee08@naver.com>
2026-09-17 23:49:53 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -0,0 +1,15 @@
+def solution(my_string):
+    arr = my_string.split()
+    
+    answer = int(arr[0])
+    
+    for i in range(1, len(arr), 2):
+        operator = arr[i]
+        number = int(arr[i + 1])
+        
+        if operator == "+":
+            answer += number
+        elif operator == "-":
+            answer -= number
+            
+    return answer
